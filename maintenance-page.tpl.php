056b9211174bbc3ac0ce7da4fa5f606691f3612c
PJ McCormick <pj@mynameispj.com>
2012-03-23 07:28:17 -0700

    Starter files for Drupal 7 theme development, customized for my personal worfklow

---
+++ maintenance-page.tpl.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page while offline.
+ *
+ * All the available variables are mirrored in html.tpl.php and page.tpl.php.
+ * Some may be blank but they are provided for consistency.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_maintenance_page()
+ */
+?>
+<!doctype html>
+
+<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!-- the "no-js" class is for Modernizr. -->
+
+<head profile="<?php print $grddl_profile; ?>">
+	<?php print $head; ?>
+	<title><?php print $head_title; ?></title>
+	<?php print $styles; ?>
+	<?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>">
+	
+	<!--Maintenance page stuff goes here --> 
+
+</body>
+</html>
